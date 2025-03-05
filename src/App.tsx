a407d3065b1487f438d343fd55afd94ed9f8cccc
Fernando Lana <loslanas@gmail.com>
2025-03-05 15:33:41 -0300

    Criar página Perfil

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 import Footer from './components/footer/Footer'
+import Perfil from './pages/perfil/Perfil'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/perfil" element={<Perfil />} />
             </Routes>
           </div>
         </div>
