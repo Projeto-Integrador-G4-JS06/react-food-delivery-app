8600f08fbcaa20782e2cdf16f1901d12c154af1f
Fernando Lana <loslanas@gmail.com>
2025-03-05 17:42:35 -0300

    Merge

--- src/pages/perfil/Perfil.tsx
+++ src/pages/perfil/Perfil.tsx
@@ -17,7 +17,7 @@ function Perfil() {
 
 	return (
 		<div className="flex justify-center mx-4">
-			<div className="container mx-auto my-4 rounded-2xl overflow-hidden">
+			<div className="container min-h-[100vh] mx-auto my-4 rounded-2xl overflow-hidden">
 				<img
 					className="w-full h-72 object-cover border-b-8 border-white"
 					src="https://ik.imagekit.io/22g34n0mo/Restaurantes/foodu.jpg?updatedAt=1741200397225"
@@ -26,13 +26,13 @@ function Perfil() {
 
 				<img
 					className="rounded-full w-56 mx-auto mt-[-8rem] border-8 border-white relative z-10"
-					src={usuario.foto}
+					src= "https://ik.imagekit.io/22g34n0mo/Restaurantes/Captura%20de%20tela%202025-03-05%20155134.png?updatedAt=1741202097438" // src={usuario.foto}
 					alt={`Foto de perfil de ${usuario.nome}`}
 				/>
 
 				<div
 					className="relative mt-[-6rem] h-72 flex flex-col 
-                    bg-[#CD533B] text-white text-2xl items-center justify-center"
+                    bg-[#CD533B] md:h-80  rounded-b-2xl text-white text-2xl items-center justify-center"
 				>
 					<p>Nome: {usuario.nome} </p>
 					<p>Email: {usuario.usuario}</p>
