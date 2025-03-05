dd8cd6c8200130539de64d18931154d4fa33afb1
Isis Yume Okamoto <isis.yume2508@gmail.com>
2025-03-05 13:43:27 -0300

    :recycle: refactor componente home temporário

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/navbar/Navbar'
-import Home from './pages/Home'
+
 import Footer from './components/footer/Footer'
+import Home from './pages/home/Home'
 
 function App() {
   return (
