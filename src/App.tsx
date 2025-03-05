560cbdd97e8bff57619f8e6134135c761e9fac30
Isis Yume Okamoto <isis.yume2508@gmail.com>
2025-03-05 13:44:54 -0300

    :recycle: refactor pasta home

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/navbar/Navbar'
-import Home from './pages/Home'
+import Home from './pages/home/Home'
 import Footer from './components/footer/Footer'
 
 function App() {
