18c56f4c4158ff46ed921a9103152a545fc84a32
Jhacss <henryk09.joao@gmail.com>
2025-03-05 12:53:33 -0300

    :sparkles: Componentes Models e Services

---
+++ src/models/Produto.ts
@@ -0,0 +1,16 @@
+import Categoria from './Categoria';
+import Usuario from './Usuario';
+
+export default interface Produto {
+    id: number;
+    nome_produto: string;
+    descricao: string;
+    preco: number;
+    foto: string;
+    nutri_score: string;
+    status: boolean | null;
+    criado_em: string;
+    atualizado_em: string;
+    categoria: Categoria;
+    usuario: Usuario;
+}
