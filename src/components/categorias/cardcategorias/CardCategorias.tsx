5e90c86d867809767dbdc08939c9531f459a037e
alanbrunoscience <alanbrunomath@gmail.com>
2025-03-06 11:16:00 -0300

    :sparkles: Update CardCategorias.tsx

--- src/components/categorias/cardcategorias/CardCategorias.tsx
+++ src/components/categorias/cardcategorias/CardCategorias.tsx
@@ -10,7 +10,7 @@ function CardCategorias({ categoria }: CardCategoriaProps) {
   return (
     <div className='flex flex-col justify-between overflow-hidden border border-yellow-400 rounded-2xl drop-shadow-xl h-full transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-lg'>
       <header className='flex justify-start gap-4 items-center px-4 text-2xl text-white bg-yellow-500 font-heading'>
-        <img src={`${categoria.icone}`} alt="Category Icon" className="my-2 h-12 max-w-75" />
+        <img src={`${categoria.icone}`} alt="Category Icon" className="my-2 h-10 max-w-75" />
         <p className='text-base lg:text-lg text-gray-800 font-medium font-[family-name:var(--font-heading)]'>{categoria.nome_categoria}</p>
       </header>
       <p className='text-sm lg:text-base flex justify-center text-center items-center h-full p-8 bg-[var(--color-beige-600)] font-[family-name:var(--font-body)]'>{categoria.descricao}</p>
