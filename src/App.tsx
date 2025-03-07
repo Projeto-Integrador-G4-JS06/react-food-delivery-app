2dbebc3f2f350581ce9c2df32348d0dcf09b937e
Jhacss <henryki09.joao@gmail.com>
2025-03-07 13:46:20 -0300

    :bug: Cadastro Usuário corrigido

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Footer from './components/footer/Footer'
-import Navbar from './components/navbar/Navbar'
-import Home from './pages/home/Home'
-import Login from './pages/login/Login'
-import Cadastro from './pages/cadastro/Cadastro'
-import { AuthProvider } from './contexts/AuthContext'
-import ListaTemas from './components/temas/listatemas/ListaTemas'
-import FormTema from './components/temas/formtema/FormTema'
-import DeletarTema from './components/temas/deletartema/DeletarTema'
-import ListaPostagens from './components/postagens/listapostagens/ListaPostagens'
-import FormPostagem from './components/postagens/formpostagem/FormPostagem'
-import DeletarPostagem from './components/postagens/deletarpostagem/DeletarPostagem'
-import Perfil from './pages/perfil/Perfil'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
+import Footer from "./components/footer/Footer";
+import { AuthProvider } from "./contexts/AuthContext";
+import { ToastContainer } from "react-toastify";
+import Login from "./pages/login/Login";
+import Perfil from "./pages/perfil/Perfil";
+import ListaProdutos from "./components/produtos/listaprodutos/ListaProdutos";
+import Cadastro from "./pages/cadastro/Cadastro";
+
 
 function App() {
   return (
@@ -23,28 +18,24 @@ function App() {
         <ToastContainer />
         <BrowserRouter>
           <Navbar />
-          <div className="min-h-[80vh]">
-            <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/cadastro" element={<Cadastro />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/temas" element={<ListaTemas />} />
-              <Route path="/cadastrartema" element={<FormTema />} />
-              <Route path="/editartema/:id" element={<FormTema />} />
-              <Route path="/deletartema/:id" element={<DeletarTema />} />
-              <Route path="/postagens" element={<ListaPostagens />} />
-              <Route path="/cadastrarpostagem" element={<FormPostagem />} />
-              <Route path="/editarpostagem/:id" element={<FormPostagem />} />
-              <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
-              <Route path="/perfil" element={<Perfil />} />
-            </Routes>
+
+          <div className="flex flex-col min-h-[80vh] bg-[#F5F5DC] ">
+            <div className="flex-grow">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/perfil" element={<Perfil />} />
+                <Route path="/cadastro" element={<Cadastro />} />
+                <Route path="/produtos" element={<ListaProdutos />} />
+              </Routes>
+            </div>
           </div>
           <Footer />
         </BrowserRouter>
       </AuthProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
