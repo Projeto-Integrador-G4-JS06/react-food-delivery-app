d9a3066f2800909650baa915e328e21cf6dd124f
alanbrunoscience <alanbrunomath@gmail.com>
2025-03-06 22:31:30 -0300

    :sparkles: Update ListarCategorias

--- src/components/categorias/listarcategorias/ListarCategorias.tsx
+++ src/components/categorias/listarcategorias/ListarCategorias.tsx
@@ -50,14 +50,14 @@ function ListaCategorias() {
                     />
                 </div>
             )}
-            <div className="w-full py-8 flex justify-center items-center">
+            <div className="w-full flex justify-center items-center">
                 <div className="container flex flex-col mx-4">
                     {(!isLoading && categorias.length === 0) && (
                         <span className="my-8 text-3xl text-center">
                             Nenhuma categoria foi encontrada!
                         </span>
                     )}
-                    <section className="container w-full mx-autopx-4 flex flex-col justify-center items-center gap-10">
+                    <section className="container w-full mx-auto px-4 flex flex-col justify-center items-center gap-10">
                         <div className="w-full h-30 flex justify-between px-8 items-center bg-[var(--color-olive-500)] text-white">
                             <p className="text-2xl font-[family-name:var(--font-heading)]">Categorias</p>
                             <Link to={`/cadastrarcategoria`}>
