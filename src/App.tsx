95082e7d15c4b0c18e81fbea415a6feef4a8ed30
alanbrunoscience <alanbrunomath@gmail.com>
2025-03-06 21:39:02 -0300

    :sparkles: Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Footer from './components/footer/Footer'
-import Navbar from './components/navbar/Navbar'
-import Home from './pages/home/Home'
-import Login from './pages/login/Login'
-import Cadastro from './pages/cadastro/Cadastro'
-import { AuthProvider } from './contexts/AuthContext'
-import ListaTemas from './components/temas/listatemas/ListaTemas'
-import FormTema from './components/temas/formtema/FormTema'
-import DeletarTema from './components/temas/deletartema/DeletarTema'
-import ListaPostagens from './components/postagens/listapostagens/ListaPostagens'
-import FormPostagem from './components/postagens/formpostagem/FormPostagem'
-import DeletarPostagem from './components/postagens/deletarpostagem/DeletarPostagem'
-import Perfil from './pages/perfil/Perfil'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
+import Footer from "./components/footer/Footer";
+import { AuthProvider } from "./contexts/AuthContext";
+import { ToastContainer } from "react-toastify";
+import Login from "./pages/login/Login";
+import Perfil from "./pages/perfil/Perfil";
+import Sobre from "./pages/sobre/Sobre";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import DeletarProduto from "./components/produtos/deletarproduto/DeletarProduto";
+import ListaProdutos from "./components/produtos/listaprodutos/ListaProdutos";
+import ListaProdutosSaudaveis from "./components/produtos/listaprodutos/ListaProdutosSaudaveis";
+import FormProdutos from "./components/produtos/formprodutos/FormProdutos";
+import ListaCategorias from "./components/categorias/listarcategorias/ListarCategorias";
+import FormCategoria from "./components/categorias/formcategoria/FormCategoria";
+import DeletarCategoria from "./components/categorias/deletarcategoria/DeletarCategoria";
+
 
 function App() {
   return (
@@ -23,28 +26,29 @@ function App() {
         <ToastContainer />
         <BrowserRouter>
           <Navbar />
-          <div className="min-h-[80vh]">
+          <div className="scroll-smooth antialiased min-h-[80vh]">
             <Routes>
-              <Route path="/" element={<Login />} />
+              <Route path="/" element={<Home />} />
               <Route path="/home" element={<Home />} />
-              <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/login" element={<Login />} />
-              <Route path="/temas" element={<ListaTemas />} />
-              <Route path="/cadastrartema" element={<FormTema />} />
-              <Route path="/editartema/:id" element={<FormTema />} />
-              <Route path="/deletartema/:id" element={<DeletarTema />} />
-              <Route path="/postagens" element={<ListaPostagens />} />
-              <Route path="/cadastrarpostagem" element={<FormPostagem />} />
-              <Route path="/editarpostagem/:id" element={<FormPostagem />} />
-              <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
               <Route path="/perfil" element={<Perfil />} />
+              <Route path="/sobre" element={<Sobre />} />
+              <Route path="/categorias" element={<ListaCategorias />} />
+              <Route path="/cadastrarcategoria" element={<FormCategoria />} />
+              <Route path="/editarcategoria/:id" element={<FormCategoria />} />
+              <Route path="/deletarcategoria/:id" element={<DeletarCategoria />} />
+              <Route path="/produtos" element={<ListaProdutos />} />
+              <Route path="/produto/:id" element={<DeletarProduto />} />
+              <Route path="/produtos/healthy" element={<ListaProdutosSaudaveis />} />
+              <Route path="/cadastrarproduto" element={<FormProdutos />} />
+              <Route path="/atualizarproduto/:id" element={<FormProdutos />} />
             </Routes>
           </div>
           <Footer />
-        </BrowserRouter>
-      </AuthProvider>
+        </BrowserRouter >
+      </AuthProvider >
     </>
-  )
+  );
 }
 
-export default App
+export default App;
