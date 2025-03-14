d34387d1d3e3f53219c71641ee0741a2c74072f5
Samuel <samuka2568@gmail.com>
2025-03-14 18:48:09 -0300

    :sparkles: Juntar código com a main

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: import.meta.env.VITE_API_URL,
-   baseURL: 'https://nest-food-delivery-app.onrender.com'
+   baseURL: import.meta.env.VITE_API_URL,
+  //  baseURL: 'https://nest-food-delivery-app.onrender.com'
 });
 
 export const cadastrarUsuario = async (
